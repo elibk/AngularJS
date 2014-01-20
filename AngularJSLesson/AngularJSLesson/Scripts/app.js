fcf5f39a5058ed35399a1bb07231de93e10dd1b2
elibkirilova <elibkirilova@gmail.com>
2014-01-20 16:57:40 +0200

    initial

---
+++ AngularJSLesson/AngularJSLesson/Scripts/app.js
@@ -0,0 +1,8 @@
+﻿
+angular.module('weather-forecast', ['ngRoute', 'ngSanitize']).config(['$routeProvider',
+  function($routeProvider) {
+      $routeProvider
+            .when("/", { templateUrl: "scripts/partial-views/home.html", controller: MainController })
+			.when("/location", { templateUrl: "scripts/partial-views/location.html", controller: MainController })
+            .otherwise({ redirectTo: '/' });
+    }]);
---
+++ AngularJSLesson/AngularJSLesson/Scripts/app/controllers.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="../angular.js" />
+//http://freegeoip.net/json
+
+function MainController($scope, $http, $sce) {
+   
+    $scope.countyInfo = "";
+    $scope.html = "";
+
+    $scope.position = function () {
+        navigator.geolocation.getCurrentPosition(
+          function (position) {
+
+            var yahooGeoPlaceUrl = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20geo.placefinder%20where%20text%3D%22' + position.coords.latitude + '%2C' + position.coords.longitude + '%22%20and%20gflags%3D%22R%22&format=json';
+            $http({ method: 'GET', url: yahooGeoPlaceUrl }).
+                    success(function (country) {
+                        var urlYahooo = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%3D' + country.query.results.Result.woeid + '&diagnostics=true&format=json';
+                        $http({ method: 'GET', url: urlYahooo }).
+                            success(function (country) {
+                                $scope.countyInfo = country;
+                                $scope.html = $sce.trustAsHtml(country.query.results.channel.item.description);
+                            }).
+                            error(function (error) {
+                                console.log(error);
+                            });
+                    }).
+                    error(function (error) {
+                        console.log(error);
+                    });
+            }
+        )
+    }();
+
+   
+  }
